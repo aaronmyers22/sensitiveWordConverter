9cb5f24c56523059416c120ce8904e400c949de7
Aaron Myers <am.myers1022@gmail.com>
2021-12-09 10:12:48 +0100

    Initial Commit

---
+++ Shared/sensitiveWordConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  sensitiveWordConverterApp.swift
+//  Shared
+//
+//  Created by Aaron Myers on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct sensitiveWordConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
